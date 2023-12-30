1060eac812b1216d9a91ca0751578059d6c2411f
Stein-N <nico-stein-98@outlook.de>
2023-12-30 19:53:53 +0100

    adding Documentation for SimpleConfigRegistryImpl

--- common/src/main/java/net/xstopho/simpleconfig/api/SimpleConfigRegistryImpl.java
+++ common/src/main/java/net/xstopho/simpleconfig/api/SimpleConfigRegistryImpl.java
@@ -4,11 +4,25 @@ import net.xstopho.simpleconfig.builder.ISimpleConfigBuilder;
 import net.xstopho.simpleconfig.config.SimpleModConfig;
 
 public class SimpleConfigRegistryImpl implements SimpleConfigRegistry {
+
+    /**
+     * @param modId  ModID of the Mod utilizing this API
+     * @param fileName  Can be set to create more than one config File
+     * @param builder  Every Builder that implements the {@link ISimpleConfigBuilder} can be set.<br>
+     *                This Method get called in the Main Method of the Mod to register and create
+     *                the specified config File.
+     */
     @Override
     public void register(String modId, String fileName, ISimpleConfigBuilder builder) {
         new SimpleModConfig(modId, fileName, builder);
     }
 
+    /**
+     * @param modId  ModID of the Mod utilizing this API, also used as the File name for the config.
+     * @param builder  Every Builder that implements the {@link ISimpleConfigBuilder} can be set.<br>
+     *                This Method get called in the Main Method of the Mod to register and create
+     *                the specified config File.
+     */
     @Override
     public void register(String modId, ISimpleConfigBuilder builder) {
         new SimpleModConfig(modId, modId, builder);
