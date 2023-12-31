fd281ece11d1c623a167f1a8d6b7b4164d2e6dd6
Stein-N <nico-stein-98@outlook.de>
2023-12-31 15:20:49 +0100

    NightConfig gets shaded into Fabric JAr file

--- fabric/src/main/java/net/xstopho/simpleconfig/SimpleConfig.java
+++ fabric/src/main/java/net/xstopho/simpleconfig/SimpleConfig.java
@@ -1,11 +1,16 @@
 package net.xstopho.simpleconfig;
 
 import net.fabricmc.api.ModInitializer;
+import net.xstopho.simpleconfig.builder.SimpleConfigBuilder;
+
+import java.util.function.Supplier;
 
 public class SimpleConfig implements ModInitializer {
 
+    static SimpleConfigBuilder builder = new SimpleConfigBuilder();
+
     @Override
     public void onInitialize() {
-
+        Supplier<Integer> test = builder.define("test", 100);
     }
 }
